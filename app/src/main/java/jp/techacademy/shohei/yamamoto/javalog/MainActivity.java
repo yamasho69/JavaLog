0f2933cca7494e94a33184dc537dc42fa2c477a5
shoheiyamamoto <nitiham55@yahoo.co.jp>
2018-09-04 18:42:27 +0900

    Lesson3
    再提出

--- app/src/main/java/jp/techacademy/shohei/yamamoto/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/shohei/yamamoto/javalog/MainActivity.java
@@ -11,12 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Human human = new Human("トム",25,"料理");//名前をトム、年齢25歳で、趣味が料理のHumanのインスタンスを作る
+        Human human = new Human("～～","～～","料理");//名前を～～、年齢～～歳で、趣味が料理のHumanのインスタンスを作る
         human.say();
         human.think();
-
-        Human human2 = new Human("クルーズ",38,"映画");//名前をクルーズ、年齢38歳で、趣味が映画のHumanのインスタンスを作る
-        human2.say();
-        human2.think();
     }
 }
